ea1a0b5fbfb204375ff1d2a0e499e6dd7e91a683
Vinicius Marcante <vinicius.marcante@kron.digital>
2022-06-22 16:34:13 -0300

    Mudança de sorting datas

--- src/app/extrato/extrato-mes/extrato-mes.component.ts
+++ src/app/extrato/extrato-mes/extrato-mes.component.ts
@@ -1,4 +1,3 @@
-import { Transacao } from 'src/app/models/transacao.model';
 import { TransacaoMes } from './../../models/transacoesMes.model';
 import { Component, Input, OnInit } from '@angular/core';
 
@@ -22,8 +21,22 @@ export class ExtratoMesComponent implements OnInit {
 
   ordernarTransacoes() {
     if (this.mes.transacoes) {
-      let mesOrdenado : any = this.mes.transacoes.slice();
+      let mesOrdenado: any = this.mes.transacoes.slice();
 
+      mesOrdenado.forEach((_array: any) => {
+        mesOrdenado.forEach((_array2: any, j: any) => {
+          let dataAtual : number = mesOrdenado[j] ? mesOrdenado[j]?.data.split('-')[2] : null;
+          let dataProx : number = mesOrdenado[j + 1] ? mesOrdenado[j + 1]?.data.split('-')[2] : null;
+          if (dataAtual != null && dataProx != null && dataAtual > dataProx) {
+            let troca = mesOrdenado[j];
+            mesOrdenado[j] = mesOrdenado[j + 1];
+            mesOrdenado[j + 1] = troca;
+          }
+        });
+      });
+
+      return mesOrdenado;
+      /*
       for (let i = 0; i < mesOrdenado.length; i++) {
         for (let j = 0; j < mesOrdenado.length - 1; j++) {
           let dataAtual: number = mesOrdenado[j].data.split('-')[2];
@@ -35,8 +48,9 @@ export class ExtratoMesComponent implements OnInit {
           }
         }
       }
+      */
 
-      return mesOrdenado;
+
     }
   }
 }
